e430c9dd3fe1080ea30be833de303b8fef465947
Hebatullah Dwairi <hebamahmoud8843@gmail.com>
2025-05-24 13:22:10 +0300

    replace TodoViewSet with APIView based endpoints

--- todos/urls.py
+++ todos/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from .views import TodoViewSet
+from .views import TodoView, TodoDetailView
 from rest_framework.routers import DefaultRouter
 from django.urls import include
 
-router = DefaultRouter()
-router.register(r'todos', TodoViewSet, basename='todo')
-
 urlpatterns = [
-  path('', include(router.urls)),
+  path('todos/', TodoView.as_view()),
+  path('todos/<int:pk>', TodoDetailView.as_view())
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,11 +1,61 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .serializers import TodoSerializer
-from rest_framework import generics, permissions
+from rest_framework import status, permissions
 from .models import Todo
 from rest_framework import viewsets
 from .permissions import IsOwner
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404
 
+
+class TodoView(APIView):
+  permission_classes = (permissions.IsAuthenticated, IsOwner)
+
+  def get(self, request):
+    todos = Todo.objects.filter(user=request.user)
+    serializer = TodoSerializer(todos, many=True)
+    return Response(serializer.data)
+  
+  def post(self, request):
+    serializer = TodoSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save(user=request.user)
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TodoDetailView(APIView):
+  permission_classes = (permissions.IsAuthenticated, IsOwner)
+
+  def get_object(self, pk):
+    todo = get_object_or_404(Todo, pk=pk, user=self.request.user)
+    self.check_object_permissions(self.request, todo)
+    return todo
+
+  def get(self, request, pk):
+    todo = self.get_object(pk)
+    serializer = TodoSerializer(todo)
+    return Response(serializer.data)
+  
+  def put(self, request, pk):
+    todo = self.get_object(pk)
+    serializer = TodoSerializer(todo, data=request.data)
+    if serializer.is_valid():
+        serializer.save(user=request.user)
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+  def delete(self, request, pk):
+    todo = self.get_object(pk)
+    todo.delete()
+
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+'''
+# using ModelViewSet instead
 class TodoViewSet(viewsets.ModelViewSet):
   queryset = Todo.objects.all()
   serializer_class = TodoSerializer
@@ -16,3 +66,5 @@ class TodoViewSet(viewsets.ModelViewSet):
   
   def get_queryset(self):
     return self.queryset.filter(user=self.request.user)
+
+'''
